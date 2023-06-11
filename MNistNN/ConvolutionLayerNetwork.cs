fe4cab1c98445e2fdcb83bf8a26a00497a1438b5
Savan Dosanjh <sc21srsd@leeds.ac.uk>
2023-06-11 22:26:21 +0100

    Added feedforward step to convolutional net

--- MNistNN/ConvolutionLayerNetwork.cs
+++ MNistNN/ConvolutionLayerNetwork.cs
@@ -9,13 +9,13 @@ namespace MNistNN
     internal class ConvolutionLayerNetwork
     {
         int layers;
-        List<double[,,,]> kernels;
+        List<double[,,,]> kernels; 
         List<double[,,]> activation;
         List<double[,,]> bias;
 
         //Assumed convolutional-pooling pattern for structure
         //NOTE: kernel sets must be same depth as input 
-        public ConvolutionLayerNetwork(int iX, int iY, int iZ, int[,] structure)
+        public ConvolutionLayerNetwork(int iX, int iY, int iZ, int[,] structure, double[,,] input)
         {
             //Network structure initialisation
             layers = structure.GetLength(0) + 1;
@@ -26,13 +26,15 @@ namespace MNistNN
             activation.Add(new double[iX, iY, iZ]);
             bias.Add(new double[iX, iY, iZ]);
 
+            activation[0] = input;
+
             for(int i = 0; i < structure.GetLength(0); i++)
             {
                 kernels.Add(new double[structure[i, 0],
                 iZ, structure[i, 1], structure[i, 2]]);
 
-                iX -= structure[i, 0] - 1;
-                iY -= structure[i, 1] - 1;
+                iX -= (structure[i, 0] - 1);
+                iY -= (structure[i, 1] - 1);
                 iZ = structure[i, 2];
 
                 activation.Add(new double[iX, iY, iZ]);
@@ -45,6 +47,74 @@ namespace MNistNN
 
             //Kernel Randomisation
             randomiseKernel();
+        }
+
+        //Feedforward
+        public void Run()
+        {
+            for(int i = 1; i < layers; i++)
+            {
+                //Compute for each kernel set
+                for(int k = 0; k < kernels[i - 1].GetLength(0); k++)
+                {
+                    //Convolve and sum for each depth of the input
+                    for (int x = 0; x < activation[i - 1].GetLength(0); x++)
+                    {
+                        double[,,] conv = convolution(k, x, 
+                        kernels[i - 1], activation[i - 1]);
+                        sum(k, 0, activation[i], conv);
+                    }
+                    //Apply bias and activation to layer
+                    sum(k, k, activation[i], bias[i], true);
+                }
+            }
+        }
+
+        //Activation function
+        private double A(double z)
+        {
+            return Math.Max(0, z);
+            return 1 / (1 + Math.Pow(Math.E, z * -1));
+        }
+
+        private double[,,] convolution(int a, int b, double[,,,] m1, double[,,] m2)
+        {
+            int k1 = (m1.GetLength(0) - m2.GetLength(0)) + 1;
+            int k2 = (m1.GetLength(1) - m2.GetLength(1)) + 1;
+            double[,,] result = new double[1, k1, k2];
+
+            for(int x = 0; x < k1; x++)
+            {
+                for(int y = 0; y < k2; y++)
+                {
+                    double sum = 0;
+                    for(int z = k1; z < k1 + m2.GetLength(0); z++)
+                    {
+                        for(int w = k2; w < k2 + m2.GetLength(1); w++)
+                        {
+                            sum += m1[a, b, z, w] * m2[b, z - k1, w - k2];
+                        }
+                    }
+                    result[0, x, y] = sum;
+                }
+            }
+
+            return result;
+        }
+
+        private void sum(int a, int b, double[,,] m1, double[,,] m2, bool activ=false)
+        {
+            for(int x = 0; x < m1.GetLength(1); x++)
+            {
+                for(int y = 0; y < m1.GetLength(2); y++)
+                {
+                    m1[a, x, y] += m2[b, x, y];
+                    if (activ)
+                    {
+                        m1[a, x, y] = A(m1[a, x, y]);
+                    }
+                }
+            }
         }
 
         private void randomiseBias()
