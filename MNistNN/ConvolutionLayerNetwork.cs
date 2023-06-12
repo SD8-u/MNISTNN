39f32b36ba0e3d98fccab5953ffd8f739d7629b8
Savan Dosanjh <sc21srsd@leeds.ac.uk>
2023-06-12 01:53:19 +0100

    Added max-pooling for forward propagation

--- MNistNN/ConvolutionLayerNetwork.cs
+++ MNistNN/ConvolutionLayerNetwork.cs
@@ -12,23 +12,25 @@ namespace MNistNN
         List<double[,,,]> kernels; 
         List<double[,,]> activation;
         List<double[,,]> bias;
+        bool[] convPoolConfig;
 
-        //Assumed convolutional-pooling pattern for structure
         //NOTE: kernel sets must be same depth as input 
-        public ConvolutionLayerNetwork(int iX, int iY, int iZ, int[,] structure, double[,,] input)
+        public ConvolutionLayerNetwork(int iX, int iY, int iZ, int[,] structure, 
+        double[,,] input, bool[] convPoolConfig)
         {
             //Network structure initialisation
             layers = structure.GetLength(0) + 1;
             kernels = new List<double[,,,]>();
             activation = new List<double[,,]>();
             bias = new List<double[,,]>();
+            this.convPoolConfig = convPoolConfig;
 
             activation.Add(new double[iX, iY, iZ]);
             bias.Add(new double[iX, iY, iZ]);
 
             activation[0] = input;
 
-            for(int i = 0; i < structure.GetLength(0); i++)
+            for (int i = 0; i < structure.GetLength(0); i++)
             {
                 kernels.Add(new double[structure[i, 0],
                 iZ, structure[i, 1], structure[i, 2]]);
@@ -47,6 +49,7 @@ namespace MNistNN
 
             //Kernel Randomisation
             randomiseKernel();
+            this.convPoolConfig = convPoolConfig;
         }
 
         //Feedforward
@@ -54,18 +57,54 @@ namespace MNistNN
         {
             for(int i = 1; i < layers; i++)
             {
-                //Compute for each kernel set
-                for(int k = 0; k < kernels[i - 1].GetLength(0); k++)
+                //Convolutional layer
+                if (convPoolConfig[i - 1])
                 {
-                    //Convolve and sum for each depth of the input
-                    for (int x = 0; x < activation[i - 1].GetLength(0); x++)
+                    //Compute for each kernel set
+                    for (int k = 0; k < kernels[i - 1].GetLength(0); k++)
                     {
-                        double[,,] conv = convolution(k, x, 
-                        kernels[i - 1], activation[i - 1]);
-                        sum(k, 0, activation[i], conv);
+                        //Convolve and sum for each depth of the input
+                        for (int x = 0; x < activation[i - 1].GetLength(0); x++)
+                        {
+                            double[,,] conv = convolution(k, x,
+                            kernels[i - 1], activation[i - 1]);
+                            sum(k, 0, activation[i], conv);
+                        }
+                        //Apply bias and activation to layer
+                        sum(k, k, activation[i], bias[i], true);
                     }
-                    //Apply bias and activation to layer
-                    sum(k, k, activation[i], bias[i], true);
+                }
+                //Pooling layer
+                else
+                {
+                    for(int a = 0; a < activation[i - 1].GetLength(0); a++)
+                    {
+                        maxPool(i, a);
+                    }
+                }
+            }
+        }
+
+        //Max pooling function
+        private void maxPool(int i, int a)
+        {
+            int k1 = kernels[i - 1].GetLength(2);
+            int k2 = kernels[i - 1].GetLength(3);
+            for (int b = 0; b < activation[i - 1].GetLength(1)
+            - (k1 - 1); b += k1 + 1)
+            {
+                for (int c = 0; c < activation[i - 1].GetLength(2)
+                - (k2 - 1); c += k2 + 1)
+                {
+                    double max = Double.MinValue;
+                    for (int x = b; x < b + kernels[i - 1].GetLength(2); x++)
+                    {
+                        for (int y = c; y < c + kernels[i - 1].GetLength(3); y++)
+                        {
+                            max = Math.Max(max, activation[i - 1][a, x, y]);
+                        }
+                    }
+                    activation[i][a, b / (k1 + 1), c / (k2 + 1)] = max;
                 }
             }
         }
@@ -79,8 +118,8 @@ namespace MNistNN
 
         private double[,,] convolution(int a, int b, double[,,,] m1, double[,,] m2)
         {
-            int k1 = (m1.GetLength(0) - m2.GetLength(0)) + 1;
-            int k2 = (m1.GetLength(1) - m2.GetLength(1)) + 1;
+            int k1 = (m1.GetLength(2) - m2.GetLength(1)) + 1;
+            int k2 = (m1.GetLength(3) - m2.GetLength(2)) + 1;
             double[,,] result = new double[1, k1, k2];
 
             for(int x = 0; x < k1; x++)
@@ -88,9 +127,9 @@ namespace MNistNN
                 for(int y = 0; y < k2; y++)
                 {
                     double sum = 0;
-                    for(int z = k1; z < k1 + m2.GetLength(0); z++)
+                    for(int z = k1; z < k1 + m2.GetLength(1); z++)
                     {
-                        for(int w = k2; w < k2 + m2.GetLength(1); w++)
+                        for(int w = k2; w < k2 + m2.GetLength(2); w++)
                         {
                             sum += m1[a, b, z, w] * m2[b, z - k1, w - k2];
                         }
